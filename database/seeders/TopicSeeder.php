cdae0a1f1551e6082ebe1dc24b960796b6849a3a
Sabacha <mezzoforte.mackerel@icloud.com>
2023-08-20 03:55:51 +0900

    feat: seederに対応

---
+++ database/seeders/TopicSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeders;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Topic;
+
+class TopicSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Topic::factory(10)->create();
+    }
+}
